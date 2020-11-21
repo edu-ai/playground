9ad8abebe6133bf90e99ccdb137a14da35a84eb5
vineethbuddha <buddhavineeth@gmail.com>
2020-11-21 18:30:08 +0800

    include student matric number in output

--- testbed/search/search_run.py
+++ testbed/search/search_run.py
@@ -32,9 +32,9 @@ def main(env_setup_dict):
     board = initial_state[0]['board']
     initial_position = initial_state[0]['position']
 
-    s = Search(board, initial_position)
-    m = s.matric_num
-    actions = s.search()
+    search_program = Search(board, initial_position)
+    matric_number = search_program.matric_num
+    actions = search_program.search()
 
     # Monitor time elapsed
     current_time = time.time()
@@ -43,7 +43,7 @@ def main(env_setup_dict):
     # Terminate program if time limit exceeded
     if elapsed_time > time_limit:
         print("Your algorithm has exceeded the time limit.")
-        return (elapsed_time, '-', done)
+        return (matric_number, elapsed_time, '-', done)
 
     # Iterate through agent actions and environment observations
     for i in range(int(env_setup_dict['max_steps'])):
@@ -55,7 +55,7 @@ def main(env_setup_dict):
         env.render()
         state, reward, done, info = env.step(curr_action)
 
-    return (elapsed_time, len(actions), done)
+    return (matric_number, elapsed_time, len(actions), done)
 
     env.close()
 
@@ -67,17 +67,19 @@ if __name__ == '__main__':
 
     # Execute main runner script and collect results
     result = main(env_setup_dict)
-    time_taken = result[0]
-    num_steps = result[1]
-    hasSucceeded = result[2]
+    matric_number = result[0]
+    time_taken = result[1]
+    num_steps = result[2]
+    hasSucceeded = result[3]
 
     # Output statistics into a CSV file
     with open('output_file.csv', mode='w') as csv_file:
-        fieldnames = ['Success?', 'Timing (seconds)', 'Number of Steps']
+        fieldnames = ['Matric Number', 'Success?',
+                      'Timing (seconds)', 'Number of Steps']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
         writer.writeheader()
-        writer.writerow({'Success?': hasSucceeded,
+        writer.writerow({'Matric Number': matric_number, 'Success?': hasSucceeded,
                          'Timing (seconds)': time_taken, 'Number of Steps': num_steps})
 
     # Also print outcome on terminal
